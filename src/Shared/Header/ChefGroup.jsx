f7b2610b371741e0f52120c9db2e6a11eea0cc88
ArjumanJesmin <arjumanjesmin221186@gmail.com>
2023-05-03 01:17:19 +0600

    json data is added done

--- src/Shared/Header/ChefGroup.jsx
+++ src/Shared/Header/ChefGroup.jsx
@@ -1,10 +1,19 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function ChefGroup() {
+
+    const [allShef, setAllShef] = useState('');
+
+    useEffect(() => {
+        fetch('https://shef-sarver-side-arjumanjesmin.vercel.app/allData')
+        .then(res=> res.json())
+        .then(data=> setAllShef(data))
+    }, [])
+
     return (
         <div>
-            <h2>All card is Coming soon....................</h2>
+            <h2>{allShef.length}</h2>
         </div>
     );
 }
