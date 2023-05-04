bb17bb9c3fbb5fbb7e71a27aae516131d41005f2
ArjumanJesmin <arjumanjesmin221186@gmail.com>
2023-05-04 18:44:58 +0600

    updated done

--- src/Pages/Home/Recipes.jsx
+++ src/Pages/Home/Recipes.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Card, Container} from 'react-bootstrap';
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 import Items from './Items';
@@ -8,9 +8,9 @@ import { ToastContainer, toast } from 'react-toastify';
 const Recipes = () => {
     const data = useLoaderData();
     const [accepted, setAccepted] = useState(false)
-    const {image, name,like,description} = data ;
+    const { image, name, like, description } = data;
 
-    const handleAccepted = () =>{
+    const handleAccepted = () => {
         setAccepted(true)
         toast.success('Thank You So Much!');
     }
@@ -18,19 +18,23 @@ const Recipes = () => {
     return (
         <>
             <Container>
-                <Card  className="text-center mx-auto my-5" style={{ width: '44rem' }}>
-                    <h2 className='pt-2'>{name}</h2>
-                    <Card.Img className='p-2' variant="top" src={image} />
-                    <Card.Body>
-                        <Card.Title>Special title treatment</Card.Title>
-                        <Card.Text>Description: {description} </Card.Text>
-                        <Card.Text>Like: {like} </Card.Text>
-                        <Button  disabled={accepted} onClick={handleAccepted} variant="danger">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                <ToastContainer />
+                <Row>
+                    <Col lg={8} md={6} sm={4} className=' mx-auto'>
+                        <Card className="text-center my-5">
+                            <h2 className='pt-2'>{name}</h2>
+                            <Card.Img className='p-2' variant="top" src={image} />
+                            <Card.Body>
+                                <Card.Title>Special title treatment</Card.Title>
+                                <Card.Text>Description: {description} </Card.Text>
+                                <Card.Text>Like: {like} </Card.Text>
+                                <Button disabled={accepted} onClick={handleAccepted} variant="danger">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                        <ToastContainer />
+                    </Col>
+                </Row>
             </Container>
-            <Items/>
+            <Items />
         </>
     );
 };
